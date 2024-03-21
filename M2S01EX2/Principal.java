8c8d7f6fbfa0195a486d2d23e2e897a3adcb361a
Rodrigo Antunes <antunestubarao@gmail.com>
2024-03-20 21:46:28 -0300

    M2S01EX02

---
+++ M2S01EX2/Principal.java
@@ -0,0 +1,16 @@
+package M2S01EX2;
+
+public class Principal {
+
+    public static void main(String[] args) throws Exception {
+
+        String nome = "R";
+        int idade = 46;
+        float altura = 1.72f;
+
+        System.out.println("Primeira Letra do meu Nome: " + nome);
+        System.out.println("Minha Idade: " + idade + " anos");
+        System.out.println("Minha Altura: " + altura);
+
+    }
+}
